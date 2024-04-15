a6291ecaedb2fd26111f9db464fdd50006ff2ee3
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-15 21:41:38 +0200

    adding image for main page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,14 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main className="min-h-screen bg-[#5DC9A8]">Hello World!</main>;
+  return (
+    <main className="min-h-screen bg-[#5DC9A8]">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Preview of PetSoft"
+        width={519}
+        height={472}
+      />
+    </main>
+  );
 }
