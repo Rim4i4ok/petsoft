15d71613b2a11fa53a138dea854eda57b9f5f45a
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-15 21:51:50 +0200

    adding content for main page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,14 +1,27 @@
+import Logo from "@/components/logo";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-[#5DC9A8]">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10 bg-[#5DC9A8] xl:flex-row">
       <Image
         src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
         alt="Preview of PetSoft"
         width={519}
         height={472}
       />
+
+      <div>
+        <Logo />
+        <h1 className="my-6 max-w-[500px] text-5xl font-semibold">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="max-w-[600px] text-2xl font-medium">
+          Use PetSoft to easily keep track of pets under your care. Get lifetime
+          access for $299.
+        </p>
+      </div>
     </main>
   );
 }
