7f56c0ab6fe543830febbfb3a8b3393bc893c48d
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-17 21:40:46 +0200

    adding branding and stats for /app/dashboard

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,5 +1,16 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 function Page() {
-  return <div>Dashboard Page</div>;
+  return (
+    <main>
+      <div className="flex items-center justify-between py-8 text-white">
+        <Branding />
+
+        <Stats />
+      </div>
+    </main>
+  );
 }
 
 export default Page;
