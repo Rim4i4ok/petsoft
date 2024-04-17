d5011e9a558e6195f4c5d8dacee50e6f33daec0c
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-17 21:30:05 +0200

    adding footer for /app

--- src/app/(app)/app/layout.tsx
+++ src/app/(app)/app/layout.tsx
@@ -7,7 +7,7 @@ function Layout({ children }: { children: React.ReactNode }) {
     <>
       <BackgroundPattern />
 
-      <div className="mx-auto max-w-[1050px] px-4">
+      <div className="mx-auto flex min-h-screen max-w-[1050px] flex-col px-4">
         <AppHeader />
         {children}
         <AppFooter />
