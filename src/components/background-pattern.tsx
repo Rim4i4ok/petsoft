181b6f0169ddab80cc6a74738e70a2d81e00063d
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-17 15:04:22 +0200

    filling background component

--- src/components/background-pattern.tsx
+++ src/components/background-pattern.tsx
@@ -1,5 +1,5 @@
 function BackgroundPattern() {
-  return <div>BackgroundPattern</div>;
+  return <div className="absolute top-0 -z-10 h-[300px] w-full bg-[#2C9676]" />;
 }
 
 export default BackgroundPattern;
