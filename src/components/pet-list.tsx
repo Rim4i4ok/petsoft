2a6ac29791310c7d72f444b8cfe839436e52fcd9
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-19 10:30:22 +0200

    adding styles for pet list item row

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,8 +1,19 @@
+import Image from "next/image";
+
 function PetList() {
   return (
     <ul className="border-b border-black/[0.08] bg-white">
       <li>
-        <button>Seri</button>
+        <button className="flex h-[70px] w-full cursor-pointer items-center gap-3 bg-white px-5 text-base transition hover:bg-[#EFF1F2] focus:bg-[#EFF1F2]">
+          <Image
+            className="object-covers rounded-full"
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+          />
+          <p className="font-semibold">Seri</p>
+        </button>
       </li>
     </ul>
   );
