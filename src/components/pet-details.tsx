02a1052dcce6ae76b943ff0fef00274883fa75cc
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-19 15:29:16 +0200

    extracting component in pet details

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { usePetContext } from "@/lib/hooks";
+import { Pet } from "@/lib/types";
 import Image from "next/image";
 
 function PetDetails() {
@@ -8,41 +9,63 @@ function PetDetails() {
 
   return (
     <section className="flex h-full w-full flex-col">
-      <div className="flex items-center border-b border-black/[0.08] bg-white px-8 py-5">
-        <Image
-          src={selectedPet?.imageUrl}
-          alt="Selected pet image"
-          height={75}
-          width={75}
-          className="h-[75px] w-[75px] rounded-full object-cover"
-        />
-
-        <h2 className="ml-5 text-3xl font-semibold leading-7">
-          {selectedPet?.name}
-        </h2>
-      </div>
-
-      <div className="flex justify-around px-5 py-10 text-center">
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
-            Owner name
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
-        </div>
-
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700">
-            Age
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
-        </div>
-      </div>
-
-      <section className="mx-8 mb-9 flex-1 rounded-md border border-black/[0.08] bg-white px-7 py-5">
-        {selectedPet?.notes}
-      </section>
+      {!selectedPet ? (
+        <EmptyView />
+      ) : (
+        <>
+          <TopBar pet={selectedPet} />
+          <MainInfo pet={selectedPet} />
+          <Notes pet={selectedPet} />
+        </>
+      )}
     </section>
   );
 }
 
 export default PetDetails;
+
+type Props = {
+  pet: Pet;
+};
+
+const EmptyView = () => (
+  <p className="flex h-full items-center justify-center text-2xl font-medium">
+    No pet selected
+  </p>
+);
+
+const TopBar = ({ pet }: Props) => (
+  <div className="flex items-center border-b border-black/[0.08] bg-white px-8 py-5">
+    <Image
+      src={pet.imageUrl}
+      alt="Selected pet image"
+      height={75}
+      width={75}
+      className="h-[75px] w-[75px] rounded-full object-cover"
+    />
+
+    <h2 className="ml-5 text-3xl font-semibold leading-7">{pet.name}</h2>
+  </div>
+);
+
+const MainInfo = ({ pet }: Props) => (
+  <div className="flex justify-around px-5 py-10 text-center">
+    <div>
+      <h3 className="text-[13px] font-medium uppercase text-zinc-700">
+        Owner name
+      </h3>
+      <p className="mt-1 text-lg text-zinc-800">{pet.ownerName}</p>
+    </div>
+
+    <div>
+      <h3 className="text-[13px] font-medium uppercase text-zinc-700">Age</h3>
+      <p className="mt-1 text-lg text-zinc-800">{pet.age}</p>
+    </div>
+  </div>
+);
+
+const Notes = ({ pet }: Props) => (
+  <section className="mx-8 mb-9 flex-1 rounded-md border border-black/[0.08] bg-white px-7 py-5">
+    {pet.notes}
+  </section>
+);
