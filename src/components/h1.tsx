de7a7dfdf13ec975dba9755ae5386178fd877586
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-21 08:54:45 +0200

    adding className to H1

--- src/components/h1.tsx
+++ src/components/h1.tsx
@@ -1,5 +1,16 @@
-function H1({ children }: { children: React.ReactNode }) {
-  return <h1 className="text-2xl font-medium leading-6">{children}</h1>;
+import { cn } from "@/lib/utils";
+
+type H1Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+function H1({ children, className }: H1Props) {
+  return (
+    <h1 className={cn("text-2xl font-medium leading-6", className)}>
+      {children}
+    </h1>
+  );
 }
 
 export default H1;
