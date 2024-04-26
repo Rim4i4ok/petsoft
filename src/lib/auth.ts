4e966b2e1168f3cd0a6ce799fb3b4b2748a48bb4
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-26 15:43:38 +0200

    adding code for provider in auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,7 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prisma from "./db";
+import bcrypt from "bcrypt";
 
 const config = {
   pages: {
@@ -8,9 +11,38 @@ const config = {
   //     maxAge: 30 * 24 * 60 * 60,
   //     strategy: "jwt",
   //   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        // runs on login
+        const { email, password } = credentials;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: email,
+          },
+        });
+        if (!user) {
+          console.log("User not found");
+          return null;
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword,
+        );
+        if (!passwordMatch) {
+          console.log("Invalid credentials");
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
   callbacks: {
     authorized: ({ request }) => {
+      // runs on every request with middleware
       const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
 
       if (isTryingToAccessApp) return false;
