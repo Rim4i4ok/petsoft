b2a1d02fe61af550b8094698abdcae47c0baf615
Rim4i4ok <rim4i4ok@gmail.com>
2024-04-30 13:06:16 +0200

    adding zod validation for logIn action

--- src/actions/user.actions.ts
+++ src/actions/user.actions.ts
@@ -2,10 +2,29 @@
 
 import { signIn, signOut } from "@/lib/auth";
 import { addNewUser } from "@/lib/utils.prisma";
+import { authSchema } from "@/lib/validations";
 import bcrypt from "bcryptjs";
 
-export async function logIn(formData: FormData) {
-  await signIn("credentials", formData);
+export async function logIn(formData: unknown) {
+  // check if formData is a FormData type
+  if (!(formData instanceof FormData)) {
+    return {
+      message: "Invalid form data.",
+    };
+  }
+
+  // convert formData to an object
+  const formDataObject = Object.fromEntries(formData.entries());
+
+  // validate to object
+  const validatedFormDataObject = authSchema.safeParse(formDataObject);
+  if (!validatedFormDataObject.success) {
+    return {
+      message: "Invalid form data.",
+    };
+  }
+
+  await signIn("credentials", validatedFormDataObject.data);
 }
 
 export async function signUp(formData: FormData) {
