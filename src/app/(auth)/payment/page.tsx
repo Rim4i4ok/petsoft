d2a7bd11cda37ca6fae07b8ac578a8d4a88627df
Rim4i4ok <rim4i4ok@gmail.com>
2024-05-01 12:06:13 +0200

    adding success status for payment

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -1,12 +1,19 @@
 import H1 from "@/components/h1";
 import PaymentBtn from "@/components/payment-btn";
 
-function Page() {
+function Page({ searchParams }) {
+  console.log(searchParams);
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>PetSoft access requires payment</H1>
 
-      <PaymentBtn />
+      {!searchParams.success && <PaymentBtn />}
+
+      {searchParams.success && (
+        <p className="text-2xl  text-green-700">
+          Payment successful! You now have lifetime access to PetSoft.
+        </p>
+      )}
     </main>
   );
 }
